0bf5313f6ded8966243dd76b00a5894386265af6
Gosuto <github@jorijnsmit.com>
2018-01-28 16:58:27 +0700

    changed default decimals for money to 2 and for bitcoin to 8

--- test/model/testFormat.js
+++ test/model/testFormat.js
@@ -3,18 +3,18 @@ import Format from './../../src/Format'
 
 describe('Testing formatter', () => {
   it('format.money', async () => {
-    assert(Format.money(1000) === '1,000')
+    assert(Format.money(1000) === '1,000.00')
     assert(Format.money(null) === '')
   })
   it('format.percent', async () => {
-    assert(Format.bitcoin(99.01) === '99.01')
-    assert(Format.bitcoin(1) === '1.00')
-    assert(Format.bitcoin(0.001) === '0.00')
-    assert(Format.bitcoin(null) === '')
+    assert(Format.percent(99.01) === '99.0')
+    assert(Format.percent(1) === '1.0')
+    assert(Format.percent(0.001) === '0.0')
+    assert(Format.percent(null) === '')
   })
   it('format.bitcoin', async () => {
-    assert(Format.bitcoin(1000.01) === '1000.01')
-    assert(Format.bitcoin(1000) === '1000.00')
+    assert(Format.bitcoin(0.123456789) === '0.12345679')
+    assert(Format.bitcoin(1) === '1.00000000')
     assert(Format.bitcoin(null) === '')
   })
   it('format.addPlusSign', async () => {
